18430affd49c3094cf1f2be254356cfd4ab5a30e
NikarashiHatsu <yourlovelydev@gmail.com>
2022-06-28 21:03:24 +0700

    chore: convert indentation to spaces

--- cli/scan-analyze.js
+++ cli/scan-analyze.js
@@ -2,15 +2,15 @@
  * @param {NS} ns
  **/
 export async function main(ns) {
-	// Disables the log
-	ns.clearLog();
-	ns.disableLog('ALL');
+    // Disables the log
+    ns.clearLog();
+    ns.disableLog('ALL');
 
     // Get the max-depth argument
-	let level = ns.args[0];
+    let level = ns.args[0];
 
     // Initiate the recursive function
-	await scanHost(ns, "home", 0, level);
+    await scanHost(ns, "home", 0, level);
 }
 
 /**
@@ -22,44 +22,44 @@ export async function main(ns) {
  **/
 async function scanHost(ns, host, level = 0, maxLevel = 1, oldHost = ["home"]) {
     // Scan all hosts available in that server
-	let hosts = await ns.scan(host);
+    let hosts = await ns.scan(host);
 
     // Exclude the old server
     // If the current server shows  ["home", "n00dles", "foodnstuff"]
     // And the `oldHost` shows      ["home"]
     // The filtered hosts would be  ["n00dles", "foodnstuff"]
-	let filteredHosts = hosts.filter(h => !oldHost.includes(h));
+    let filteredHosts = hosts.filter(h => !oldHost.includes(h));
 
     // Adds all the hosts available to the `oldHost` to prevent recursivity
-	hosts.forEach(host => oldHost.push(host));
+    hosts.forEach(host => oldHost.push(host));
 
-	for (const host of filteredHosts) {
+    for (const host of filteredHosts) {
         // Add sleep to prevent the Bitburner to freeze
-		await ns.sleep(100)
+        await ns.sleep(100)
 
         // Checks if the server has Root Access, along with the minimum level to use `hack()`.
-		let hRA = await ns.hasRootAccess(host);
-		let rHS = await ns.getServerRequiredHackingLevel(host);
-		let firstLine = `--Root Access: ${hRA ? 'YES' : 'NO'}, Required hacking skill: ${rHS}`
+        let hRA = await ns.hasRootAccess(host);
+        let rHS = await ns.getServerRequiredHackingLevel(host);
+        let firstLine = `--Root Access: ${hRA ? 'YES' : 'NO'}, Required hacking skill: ${rHS}`
 
         // Get the minimum ports that should be bypassed in order to use `hack()`
-		let sNPR = await ns.getServerNumPortsRequired(host);
-		let secondLine = `--Number of open ports required to NUKE: ${sNPR}`;
+        let sNPR = await ns.getServerNumPortsRequired(host);
+        let secondLine = `--Number of open ports required to NUKE: ${sNPR}`;
 
         // Get the server's maximum RAM
-		let sMR = await ns.getServerMaxRam(host);
-		let thirdLine = `--RAM ${sMR}`;
+        let sMR = await ns.getServerMaxRam(host);
+        let thirdLine = `--RAM ${sMR}`;
 
         // Print the data
-		ns.print("----".repeat(level) + `> ${host}`)
-		ns.print("----".repeat(level) + firstLine)
-		ns.print("----".repeat(level) + secondLine)
-		ns.print("----".repeat(level) + thirdLine)
-		ns.print("")
+        ns.print("----".repeat(level) + `> ${host}`)
+        ns.print("----".repeat(level) + firstLine)
+        ns.print("----".repeat(level) + secondLine)
+        ns.print("----".repeat(level) + thirdLine)
+        ns.print("")
 
         // Recursive
-		if (level < maxLevel - 1) {
-			await scanHost(ns, host, level + 1, maxLevel, oldHost);
-		}
-	}
+        if (level < maxLevel - 1) {
+            await scanHost(ns, host, level + 1, maxLevel, oldHost);
+        }
+    }
 }
